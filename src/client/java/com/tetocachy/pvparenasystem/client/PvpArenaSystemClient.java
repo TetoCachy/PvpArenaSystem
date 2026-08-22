a2ad855eaebf713cd68931df4776cc401b953345
TetoCachy <tetocachy@tuta.com>
2026-08-22 20:53:16 +0200

    Init

---
+++ src/client/java/com/tetocachy/pvparenasystem/client/PvpArenaSystemClient.java
@@ -0,0 +1,10 @@
+package com.tetocachy.pvparenasystem.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PvpArenaSystemClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
